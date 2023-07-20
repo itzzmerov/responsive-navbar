72637ce49b14e0ae74723db1a9623186edc1d798
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2023-07-20 23:57:28 +0800

    updated files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,8 @@
 const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
-
-const rightDiv = document.querySelector('.rightDiv');
-const rightDivImg = rightDiv.querySelector('img');
-const rightDivWidth = rightDiv.offsetWidth;
-const rightDivHeight = rightDiv.offsetHeight;
-const imgWidth = rightDivImg.offsetWidth;
-const imgHeight = rightDivImg.offsetHeight;
+const socialIcons = document.querySelectorAll('.socials i'); // Get all social icons
+const tooltip = document.getElementById('tooltip');
 
 toggleBtn.onclick = function() {
     dropDownMenu.classList.toggle('open');
@@ -16,16 +11,20 @@ toggleBtn.onclick = function() {
     toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 }
 
-function floatImage() {
-    const centerX = rightDivWidth / 2;
-    const centerY = rightDivHeight / 2;
-  
-    const moveX = Math.sin(Date.now() / 1000) * 10; // Adjust the value (10) to control the floating range
-    const moveY = Math.cos(Date.now() / 1000) * 10; // Adjust the value (10) to control the floating range
-  
-    rightDivImg.style.transform = `translate(${moveX}px, ${moveY}px)`;
-  
-    requestAnimationFrame(floatImage);
-  }
-  
-  floatImage();
+// Event listeners for showing and hiding the tooltip
+socialIcons.forEach((icon) => {
+    icon.addEventListener('mouseenter', (event) => {
+        // Get the website name from the icon's ID
+        const websiteName = event.target.id;
+        // Set the tooltip content and position it at the bottom of the mouse
+        tooltip.innerText = websiteName;
+        tooltip.style.display = 'block';
+        tooltip.style.left = event.clientX + 'px';
+        tooltip.style.top = event.clientY + 20 + 'px';
+    });
+
+    icon.addEventListener('mouseleave', () => {
+        // Hide the tooltip when the mouse leaves the icon
+        tooltip.style.display = 'none';
+    });
+});
